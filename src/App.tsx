2918fc7871261de35e1e204408c6da22acb56211
Guillermo Ruiz <gruizbel@hotmail.es>
2022-11-18 22:44:42 +0100

    Work on file processing to return 47748 results

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Layout, Upload, Button, UploadFile } from 'antd';
+import { Layout, Upload, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
-import { RcFile, UploadChangeParam } from 'antd/es/upload';
+import { RcFile } from 'antd/es/upload';
+import { Pie } from '@ant-design/plots';
 import ILogInfo from './ILogInfo';
+import IRegExLine from './IRegExLine';
 
 function App() {
 
@@ -18,12 +20,13 @@ function App() {
   }
 
   const processFile = (rawContent: string) : ILogInfo[] => {
-    
+
+    console.log("Lines: " + rawContent.split(/\r?\n/).length);
+
     const regex = /(?<host>.+)\s\[(?<date>.+)\]\s\"(?<method>\w+)\s(?<url>.+)\s(?<protocol>\w+)\/(?<version>.+)\"\s(?<code>\d+)\s(?<bytes>\d+|\-)/gm;
-
-    const result = Array.from(rawContent.matchAll(regex));    
-
-    return result.map<ILogInfo>(match => {
+    const lines = rawContent.split(/\r?\n/);
+    return lines.map<ILogInfo>(l => {
+      const match = l.match(regex);
       const groups = match?.groups;
       if(groups){
         const dateParts = groups["date"].split(":");
@@ -42,7 +45,37 @@ function App() {
         response_code: "200",
         document_size: 0      
       };
-    });    
+    });
+    
+    // const regex = /(?<host>.+)\s\[(?<date>.+)\]\s\"(?<method>\w+)\s(?<url>.+)\s(?<protocol>\w+)\/(?<version>.+)\"\s(?<code>\d+)\s(?<bytes>\d+|\-)\n/gm;
+
+    // const result = Array.from(rawContent.matchAll(regex));    
+
+    // return result.map<ILogInfo>(match => {
+    //   const groups = match?.groups;
+    //   if(groups){
+    //     const dateParts = groups["date"].split(":");
+    //     return {
+    //       host: groups["host"],
+    //       datetime: {day: dateParts[0], hour: dateParts[1], minute: dateParts[2], second: dateParts[3]},
+    //       request: {method: groups["method"], url: groups["url"], protocol: groups["protocol"], protocol_version: groups["version"]},
+    //       response_code: groups["code"],
+    //       document_size: Number(groups["bytes"])
+    //     }
+    //   }
+    //   else return {
+    //     host: "",
+    //     datetime: {day: "", hour: "", minute: "", second: ""},
+    //     request: {method: "", url: "", protocol: "", protocol_version: ""},
+    //     response_code: "200",
+    //     document_size: 0      
+    //   };
+    // });    
+  }
+
+  const getHttpMethodsChartData = (lines : ILogInfo[]) => {
+    const httpMethods = new Set([...lines.map(x => x.request.method)]);
+    return Array.from(httpMethods).map<{type: string, value: number}>(x => ({type: x, value: lines.filter(l => l.request.method === x).length}));
   }
 
   return (
@@ -53,7 +86,13 @@ function App() {
         </Upload>
       </Layout.Header>
       <Layout.Content>
-
+        {info.length > 0 ? 
+          <>
+            <Pie data={getHttpMethodsChartData(info)} angleField='value' colorField='type' />
+          </> 
+          :
+          <h1>Please upload a log file above</h1>
+        }
       </Layout.Content>
     </Layout>
   );
---
+++ src/IRegExLine.ts
@@ -0,0 +1,10 @@
+export default interface IRegExLine {
+    host: string;
+    date: string;
+    method: string;
+    url: string;
+    protocol: string;
+    version: string;
+    code: string;
+    bytes: string;
+}
